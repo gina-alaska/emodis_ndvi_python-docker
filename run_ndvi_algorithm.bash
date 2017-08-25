ca5b29aeb91c506ee701a9a99a612aa71c8e543d
Jiang Zhu <jiang@gina.alaska,edu>
2017-08-24 17:27:36 -0800

    2017/08/24/5:27

---
+++ run_ndvi_algorithm.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [$# !=1 ];then
+
+  echo "input:year"
+  exit 1
+
+fi
+
+echo "run NDVI algorithm to produce NDVI metrics"
+
+cd $HOME_EXC/scripts/ver-for-docker
+
+./1yr_emodis_250_main_py_docker.bash $year
+
+echo "done!"
+
+exit 0
